3b3ce2a7a20a1d98f9566a35b61bb2fa673aef31
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2023-09-08 16:00:05 -0700

    final

--- resources/views/todos/todo-list/edit.blade.php
+++ resources/views/todos/todo-list/edit.blade.php
@@ -29,8 +29,7 @@
                         </div>
                         <div class="form-group row m-b-0">
                             <div class="offset-sm-3 col-sm-9">
-                                <button type="submit" class="btn btn-success waves-effect waves-light text-white">Update
-                                    New Todo List
+                                <button type="submit" class="btn btn-success waves-effect waves-light text-white">Update Todo List
                                 </button>
                             </div>
                         </div>
